47a1e281946908e7a894e1ecfe1a206a44b17593
mukhlis <mukhlis270189@gmail.com>
2022-08-27 10:42:17 +0700

    tambah file index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba git remote</title>
+</head>
+<body>
+    <h3>percobaan ke </h3>
+    <p>ini adalah halama percobaan git remote yg sdh ada repo sblmnya bukan dari awal</p>
+</body>
+</html>
